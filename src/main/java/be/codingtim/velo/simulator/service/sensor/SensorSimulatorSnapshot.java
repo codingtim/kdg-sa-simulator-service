4f79469ab1f1c395075ba0ae59e1b45c50d37e5f
Tim Boeckstaens <boeckstaenstim@gmail.com>
2020-02-23 08:13:21 +0100

    make the lists inside the snapshot immutable

--- src/main/java/be/codingtim/velo/simulator/service/sensor/SensorSimulatorSnapshot.java
+++ src/main/java/be/codingtim/velo/simulator/service/sensor/SensorSimulatorSnapshot.java
@@ -2,6 +2,8 @@ package be.codingtim.velo.simulator.service.sensor;
 
 import java.util.List;
 
+import static java.util.Collections.unmodifiableList;
+
 public class SensorSimulatorSnapshot {
     public final List<SensorSimulationView> waitingSimulations;
     public final List<SensorSimulationView> runningSimulations;
@@ -10,8 +12,8 @@ public class SensorSimulatorSnapshot {
     public SensorSimulatorSnapshot(List<SensorSimulationView> waitingSimulations,
                                    List<SensorSimulationView> runningSimulations,
                                    List<SensorSimulationView> completedSimulations) {
-        this.waitingSimulations = waitingSimulations;
-        this.runningSimulations = runningSimulations;
-        this.completedSimulations = completedSimulations;
+        this.waitingSimulations = unmodifiableList(waitingSimulations);
+        this.runningSimulations = unmodifiableList(runningSimulations);
+        this.completedSimulations = unmodifiableList(completedSimulations);
     }
 }
